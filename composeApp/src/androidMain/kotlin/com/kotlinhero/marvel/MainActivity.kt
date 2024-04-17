f5f91b88515314551752f18b9257f2671dc63cb5
KotlinHero <ahmad.alshahal2@gmail.com>
2024-04-17 17:39:40 +0300

    fix
    Fixed desktop coroutines issue

--- composeApp/src/androidMain/kotlin/com/kotlinhero/marvel/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/kotlinhero/marvel/MainActivity.kt
@@ -3,6 +3,7 @@ package com.kotlinhero.marvel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import app.ui.App
@@ -10,6 +11,8 @@ import app.ui.App
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
 
         setContent {
             App()
